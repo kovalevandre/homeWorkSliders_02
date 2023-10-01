0af49aaa80b30cc692b88d3338236b9a3eba2051
Andrey <akovalev@me.com>
2023-10-01 17:48:46 +0300

    New commit after download

--- homeWorkSliders_02/ViewController.swift
+++ homeWorkSliders_02/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateColor()
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        let redValue = CGFloat(redSlider.value)
+        let greenValue = CGFloat(greenSlider.value)
+        let blueValue = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+        
+        redLabel.text = String(format: "%.2f", redValue)
+        greenLabel.text = String(format: "%.2f", greenValue)
+        blueLabel.text = String(format: "%.2f", blueValue)
+        
+        redSlider.minimumTrackTintColor = UIColor(red: 0, green: greenValue, blue: blueValue, alpha: 1.0)
+        greenSlider.minimumTrackTintColor = UIColor(red: redValue, green: 0, blue: blueValue, alpha: 1.0)
+        blueSlider.minimumTrackTintColor = UIColor(red: redValue, green: greenValue, blue: 0, alpha: 1.0)
+    }
 }
-
